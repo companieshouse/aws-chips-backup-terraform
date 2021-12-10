365a2fd5db0551adbeaa501a718999eb2315535a
Matt Stone <m.stone@kainos.com>
2021-12-10 15:46:04 +0000

    update to bucket per source account

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,8 +1,11 @@
-module "chips_backup_bucket" {
+###################################
+# Heritage-development resources 
+###################################
+module "heritage_development_chips_backup_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.11.1"
 
-  bucket = "${local.chips_backup_bucket_name}.${var.aws_account}.${var.aws_region}"
+  bucket = "heritage-dev-chips-backup.${var.aws_account}.${var.aws_region}"
   acl    = "private"
 
   block_public_acls       = true
@@ -15,7 +18,7 @@ module "chips_backup_bucket" {
   }
 
   # lifecycle_rule = [
-  #   #Long term cold backup, we need data lifecycle/tiering
+  #   #Long term cold backup, we should have data lifecycle/tiering
   # ]
 
   server_side_encryption_configuration = {
@@ -28,21 +31,121 @@ module "chips_backup_bucket" {
   }
 }
 
-module "chips_backup_bucket_policy" {
+module "heritage_development_chips_backup_bucket_policy" {
   source = "git@github.com:companieshouse/terraform-modules//aws/s3_cross_account_policy?ref=tags/1.0.99"
 
-  bucket_name   = module.resources_bucket.s3_bucket_id
+  bucket_name   = module.heritage_development_chips_backup_bucket.s3_bucket_id
   attach_policy = true
   bucket_read_accounts = [
   ]
   bucket_write_accounts = [
     local.account_ids["heritage-development"],
+  ]
+  bucket_delete_accounts = [
+  ]
+
+  // Depends on to avoid issues with conflicting operations adding bucket policy and public bock resources
+  depends_on = [module.heritage_development_chips_backup_bucket]
+}
+
+
+###################################
+# Heritage-staging resources 
+###################################
+module "heritage_staging_chips_backup_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.11.1"
+
+  bucket = "heritage-staging-chips-backup.${var.aws_account}.${var.aws_region}"
+  acl    = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = true
+  }
+
+  # lifecycle_rule = [
+  #   #Long term cold backup, we should have data lifecycle/tiering
+  # ]
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = local.chips_backup_kms_key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+module "heritage_staging_chips_backup_bucket_policy" {
+  source = "git@github.com:companieshouse/terraform-modules//aws/s3_cross_account_policy?ref=tags/1.0.99"
+
+  bucket_name   = module.heritage_staging_chips_backup_bucket.s3_bucket_id
+  attach_policy = true
+  bucket_read_accounts = [
+  ]
+  bucket_write_accounts = [
     local.account_ids["heritage-staging"],
+  ]
+  bucket_delete_accounts = [
+  ]
+
+  // Depends on to avoid issues with conflicting operations adding bucket policy and public bock resources
+  depends_on = [module.heritage_staging_chips_backup_bucket]
+}
+
+
+###################################
+# Heritage-live resources 
+###################################
+module "heritage_live_chips_backup_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.11.1"
+
+  bucket = "heritage-live-chips-backup.${var.aws_account}.${var.aws_region}"
+  acl    = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = true
+  }
+
+  # lifecycle_rule = [
+  #   #Long term cold backup, we should have data lifecycle/tiering
+  # ]
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = local.chips_backup_kms_key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+module "heritage_live_chips_backup_bucket_policy" {
+  source = "git@github.com:companieshouse/terraform-modules//aws/s3_cross_account_policy?ref=tags/1.0.99"
+
+  bucket_name   = module.heritage_live_chips_backup_bucket.s3_bucket_id
+  attach_policy = true
+  bucket_read_accounts = [
+  ]
+  bucket_write_accounts = [
     local.account_ids["heritage-live"],
   ]
   bucket_delete_accounts = [
   ]
 
   // Depends on to avoid issues with conflicting operations adding bucket policy and public bock resources
-  depends_on = [module.chips_backup_bucket]
+  depends_on = [module.heritage_live_chips_backup_bucket]
 }
