b8cede3f68735ab5646c0fb9e7d8d95944273209
Matt Stone <m.stone@kainos.com>
2022-03-04 16:02:51 +0000

    fixing outputs for kms data

--- groups/infrastructure/outputs.tf
+++ groups/infrastructure/outputs.tf
@@ -2,5 +2,9 @@
 resource "vault_generic_secret" "kms" {
   path = "aws-accounts/${var.aws_account}/kms"
 
-  data_json = jsonencode({ for name, outputs in module.kms : name => outputs["key_arn"] })
+  data_json = <<EOT
+{
+  "chipsbackup-kms-key-arn": "${module.kms["chipsbackup"].key_arn}"
+}
+EOT
 }
