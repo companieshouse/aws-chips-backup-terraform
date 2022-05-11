51987f8fc61f8127760ceb975b36bed14d082486
Marty Fox <martin.fox@kainos.com>
2022-05-11 13:07:26 +0100

    removing comma from end line

--- groups/infrastructure/s3_replication.tf
+++ groups/infrastructure/s3_replication.tf
@@ -20,32 +20,24 @@ data "aws_iam_policy_document" "allow_access_from_heritage_staging" {
     ]
 
     resources = [
-      "${module.heritage_staging_chips_backup[each.value].s3_bucket_arn}/*",
+      "${module.heritage_staging_chips_backup[each.value].s3_bucket_arn}/*"
     ]
   }
-}
 
-{
-   "Version":"2012-10-17",
-   "Id":"",
-   "Statement":[
-      {
-         "Sid":"Set permissions for objects",
-         "Effect":"Allow",
-         "Principal":{
-            "AWS":"arn:aws:iam::source-bucket-acct-ID:role/service-role/source-acct-IAM-role"
-         },
-         "Action":["s3:ReplicateObject", "s3:ReplicateDelete"],
-         "Resource":"arn:aws:s3:::destination/*"
-      },
-      {
-         "Sid":"Set permissions on bucket",
-         "Effect":"Allow",
-         "Principal":{
-            "AWS":"arn:aws:iam::source-bucket-acct-ID:role/service-role/source-acct-IAM-role"
-         },
-         "Action":["s3:List*", "s3:GetBucketVersioning", "s3:PutBucketVersioning"],
-         "Resource":"arn:aws:s3:::destination"
-      }
-   ]
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [local.account_ids["heritage-staging"]]
+    }
+
+    actions = [
+      "s3:List*", 
+      "s3:GetBucketVersioning", 
+      "s3:PutBucketVersioning"
+    ]
+
+    resources = [
+      module.heritage_staging_chips_backup[each.value].s3_bucket_arn
+    ]
+  }
 }
